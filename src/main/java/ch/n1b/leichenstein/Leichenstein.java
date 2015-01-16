44602f8f48bd785dec8848cea721a83b52922a23
Thomas Richner <thomasrichner+github@gmail.com>
2015-01-16 03:03:31 +0100

    added some convenience

--- src/main/java/ch/n1b/leichenstein/Leichenstein.java
+++ src/main/java/ch/n1b/leichenstein/Leichenstein.java
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
@@ -21,8 +23,18 @@ public class Leichenstein extends JavaPlugin{
         getLogger().warning("ALL HAIL N1B!");
         //---- load strings
         Properties stringProperties = new Properties();
+
+        if(!Files.exists(Paths.get(getDataFolder().getPath() + File.separator + STRINGS_FILE))){
+            try {
+                Files.createFile(Paths.get(getDataFolder().getPath() + File.separator + STRINGS_FILE),null);
+            } catch (IOException e) {
+                getLogger().warning("Cannot create '" + STRINGS_FILE + "' file");
+            }
+        }
+
         try {
-            stringProperties.load(new FileReader(getDataFolder().getPath() + File.separator + STRINGS_FILE));
+            stringProperties.load(new FileReader(Paths.get(getDataFolder().getPath() + File.separator +
+                    STRINGS_FILE).toFile()));
         } catch (IOException e) {
             getLogger().warning("Cannot load '" + STRINGS_FILE + "' file");
         }
